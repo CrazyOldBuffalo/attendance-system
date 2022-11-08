0cf17fe3c7b414831a8d8a657d5dbcab24497035
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-08 16:08:49 +0000

    Adding backend servers

---
+++ back-end/app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var bodyParser = require("body-parser");
+
+// The router for animals
+var animalsRouter = require('./Route/animal.routes');
+
+var app = express();
+
+// adding cors module
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/petshop', animalsRouter);
+
+//Database connection code
+const db = require("./models");
+db.mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to the database!");
+}).catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
