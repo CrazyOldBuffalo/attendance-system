65a49b87d62d8d83fbc2240649d2cbbdcd6b241b
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-10 23:46:24 +0000

    updated name of automated function in server.js

--- back-end/server.js
+++ back-end/server.js
@@ -30,10 +30,12 @@ var corsOptions = {
 require("./routes/user.routes")(app);
 require("./routes/tutor.routes")(app);
 
-const User = db.users;
-const Tutor = db.tutors;
 
-async function createuserTutor() {
+
+async function createUsers() {
+  const User = db.users;
+  const Tutor = db.tutors;
+  
   const user = new User({
     username: "Tom",
     password: "password"
@@ -50,7 +52,7 @@ async function createuserTutor() {
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(db.port, () => {
-  createuserTutor();
+  createUsers();
   console.log(`Server is running on port ${db.port}.`);
 });
 
