1daf3bfc63bc0223ec1cf70fe4a35d770eb570ec
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-10 22:44:18 +0000

    changed the port location to be in config and export it to the server via index

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -13,6 +13,7 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbconfig.url;
+db.port = dbconfig.port;
 db.users = require("./user.model")(mongoose);
 
 module.exports = db;
--- back-end/server.js
+++ back-end/server.js
@@ -30,8 +30,8 @@ var corsOptions = {
 require("./routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+const PORT = process.env.PORT || 5000;
+app.listen(db.port, () => {
+  console.log(`Server is running on port ${db.port}.`);
 });
 
