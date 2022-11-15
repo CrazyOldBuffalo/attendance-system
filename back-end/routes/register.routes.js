365bb795f33a75be1f6d7be2f95635c935328823
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-15 22:35:21 +0000

    various fixes

---
+++ back-end/routes/register.routes.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+    const registerController = require("../controllers/register.controller");
+    const router = require("express").Router();
+
+
+    router.get("/Register/Absent", registerController.MarkAbsent);
+
+    app.use("/", router);
+}
