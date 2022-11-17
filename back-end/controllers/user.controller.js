d3601e0bc5b056c907955b42b1f40b00548276f6
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-17 16:13:47 +0000

    More user controller functions

--- back-end/controllers/user.controller.js
+++ back-end/controllers/user.controller.js
@@ -66,17 +66,3 @@ exports.deleteOneUser = (req, res) => {
 
 };
 
-// // Update a Tutorial by the id in the request
-// exports.update = (req, res) => {
-  
-// };
-
-// // Delete a Tutorial with the specified id in the request
-// exports.delete = (req, res) => {
-  
-// };
-
-// // Delete all Tutorials from the database.
-// exports.deleteAll = (req, res) => {
-  
-// };
