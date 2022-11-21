426d7deede13477fcede2ad44a6841ddf21a777e
mr-jw <joshua.walley01@gmail.com>
2022-11-21 14:29:34 +0000

    Made a start on the classes; the user class is implemented but is not an ABB yet. Aim to finish them today.

--- src/Components/User.js
+++ src/Components/User.js
@@ -7,26 +7,57 @@ export default class User extends React.Component
         super(props);
         
         // set User attributes to default values.
-        this.state = 
-        {
-            username: "-",
-            password: "-",
-            email: "-",
-            telephoneNo: "0",
-            canEditModule: Boolean(false),
-            canEditCourse: Boolean(false)
-        }
+        this.state = { username: "", password: "", email: "", telephoneNo: "", canEditModule: false, canEditCourse: false };
     }
 
     // required function in Class.
     render()
     {
-        return
+        return (
+            <div>
+                <h1> {this.state.username} 's details </h1>
+                <p> password: {this.state.password} </p>
+                <p> email: {this.state.email} </p>
+                <p> telephoneNo. {this.state.telephoneNo} </p>
+                <p> canEditModule {this.state.canEditModule} </p>
+                <p> canEditModule {this.state.canEditCourse} </p>
+            </div>
+        )
     }
 
-
-    // test
     setUsername = (username) => {
         this.setState().username = username;
+    }
+
+    setPassword = (password) => {
+        this.setState().password = password;
+    }
+
+    setEmail = (email) => {
+        this.setState().email = email;
+    }
+
+    setTelephoneNo = (telephoneNo) => {
+        this.setState().telephoneNo = telephoneNo;
+    }
+
+    getUsername = () => {
+        return this.username;
+    }
+
+    getPassword = () => {
+        return this.password;
+    }
+
+    getEmail = () => {
+        return this.email;
+    }
+
+    getTelephoneNo = () => {
+        return this.telephoneNo;
+    }
+
+    getUser = () => {
+        return this.User;
     }
 }
