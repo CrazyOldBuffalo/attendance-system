02eb8381f4d57374b96f66718ba4d58bf8b27b81
CrazyOldBuffalo <tomasz.boberek@gmail.com>
2022-11-22 14:54:41 +0000

    Added App and other Axios Objects

--- src/App.js
+++ src/App.js
@@ -8,81 +8,16 @@ import SeachForAStudent from './Components/SearchForAStudent'
 import LoginScreen from './Components/LoginScreen'
 import './resources/styles/App.scss';
 import List from "./Components/List";
-import { BrowserRouter as Router, Routes, Route,NavLink,Link, useNavigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink, Link, useNavigate } from 'react-router-dom';
 import PrivateRoute from './Components/Auth/PrivateRoute';
-import Navbar from './Components/Navbar'
 
-import User from './Components/User'
+import User from './Components/User';
+
+import Navigation from './Components/Navigation';
 
 function App() {
-
-  let component
-
-  switch(window.location.pathname){
-
-    case"/":
-
-      component = <Home />
-      break
-
-    case "/HomeStudent":
-      component = <HomeStudent/>
-      break
-
-    case "/RegisterAttendance":
-      component = <RegisterAttendance/>
-      break
-
-     case "/SearchForAStudent":
-       component = <SeachForAStudent/>
-       break
-
-    case "/SearchForAttendanceRegister":
-      component = <SearchForAttendanceRegister/>
-      break
-
-    case "/ViewAttendance":
-      component = <ViewAttendance/>
-      break
-    
-    case "/LoginScreen":
-      component = <LoginScreen/>
-      break
-
-    case "/List":
-      component = <List/>
-      break
-
-  }
- 
-
-  
-    return(
-      <>
-     
-        <Navbar />
-        {component}
-        </>
-
-    ) 
- 
- 
   return (
-    <div className="App">
-
-      <h1>App</h1>
-      <User />
-
-      <Router>
-        <Routes>
-          <Route path='/Login' element={<Login/>} /> 
-          <Route path='/Home' element={<Home/>} />
-        </Routes>
-        <Link to={"/Home"}> Home</Link>
-      </Router>
-
-      
-    </div>
+    <Navigation />
   );
 }
 export default App;
